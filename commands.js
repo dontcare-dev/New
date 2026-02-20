0f430fbb545850e41662632a8979a3e07d1e1852
dontcare-dev <dontc4sr@gmail.com>
2026-02-20 21:01:54 +0530

    Create commands.js

---
+++ commands.js
@@ -0,0 +1,20 @@
+module.exports = {
+  system: [
+    "help","ping","uptime","botinfo","serverinfo","userid","roleinfo",
+    "channelinfo","stats","invite","support","config","permissions",
+    "prefix","setprefix","health"
+  ],
+
+  moderation: [
+    "ban","unban","kick","warn","warnings","clearwarnings","mute","unmute",
+    "timeout","untimeout","purge","clear","slowmode","lockdown","unlock",
+    "nick","roleadd","roleremove","hide","unhide","lockchannel",
+    "unlockchannel","massban","masskick","prune","nuke"
+  ],
+
+  config: [
+    "setwelcome","welcome","setgoodbye","goodbye","setautorole","autorole",
+    "setrules","rules","setlogs","logsstatus","setmuterole","muterole",
+    "setstaff","staff","reactionroles","rradd","rrremove","rrlist"
+  ]
+};
