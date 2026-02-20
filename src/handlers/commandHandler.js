d82ef15bdde3c09899a97d32bdb0dde356734ff7
dontcare-dev <dontc4sr@gmail.com>
2026-02-20 23:15:20 +0530

    Create src/handlers/commandHandler.js

---
+++ src/handlers/commandHandler.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+const path = require("path");
+
+module.exports = (client) => {
+  const folders = fs.readdirSync("./src/commands");
+
+  for (const folder of folders) {
+    const files = fs
+      .readdirSync(`./src/commands/${folder}`)
+      .filter(f => f.endsWith(".js"));
+
+    for (const file of files) {
+      const command = require(`../commands/${folder}/${file}`);
+      client.commands.set(command.name, command);
+    }
+  }
+
+  console.log(`[COMMANDS] Loaded ${client.commands.size} commands`);
+};
