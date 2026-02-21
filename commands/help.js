c5f2cc92e826f7e909ad98a7a9de3f31594769a2
dontcare-dev <dontc4sr@gmail.com>
2026-02-21 12:04:40 +0530

    Delete help.js

--- commands/help.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: "help",
-  execute(message, args, client) {
-    const list = [...message.client.commands.keys()].sort();
-    message.channel.send(
-      `📖 **Help Menu**\n\n**Total Commands:** ${list.length}\n\n${list.join(", ")}`
-    );
-  }
-};
